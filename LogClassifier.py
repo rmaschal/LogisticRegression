301e73645bb3873b08b0ab3546a71c52f27da607
rmaschal <robertmaschal@gmail.com>
2018-01-18 05:23:29 +0000

    Added MNIST dataset, small utility to read dataset and convert label to binary vector, and began developing classifier

--- LogClassifier.py
+++ LogClassifier.py
@@ -1,20 +1,39 @@
 import numpy
 
+from ReadMNIST import *
 from math import exp, log
 
+TRAINING_IMAGES_PATH = 'MNIST/train-images-idx3-ubyte'
+TRAINING_LABELS_PATH = 'MNIST/train-labels-idx1-ubyte'
+TESTING_IMAGES_PATH = 'MNIST/t10k-images-idx3-ubyte'
+TESTING_LABELS_PATH = 'MNIST/t10k-labels-idx1-ubyte'
+
 def get_training_data():
-    return []
+    num1, labels = read_labels(TRAINING_LABELS_PATH)
+    num2, images = read_images(TRAINING_IMAGES_PATH)
+    assert num1 == num2, "Number of training images and labels differ"
+
+    return num1, images, labels 
 
 def get_test_data():
-    return []
+    num1, labels = read_labels(TESTING_LABELS_PATH)
+    num2, images = read_images(TESTING_IMAGES_PATH)
+    assert num1 == num2, "Number of testing images and labels differ"
+    
+    return num1, images, labels
 
 def sigmoid(x):
     return 1 / (1 + exp( -x))
 
-def train_log_classifier(X, W, b):
-    # Get Gradient
-    # Update Weights
-    # Repeat until done (gradient near 0?)
+def train_log_classifier(num, X, Y, W, b):
+    for i in range(num):
+        y_p = get_prediction(X[i], W, b)
+    return []
 
 def get_prediction(X, W, b):
-    # Return 
+    return sigmoid(np.dot(W,X)+b)
+
+if __name__ == "__main__":
+    num, images, labels = get_training_data()
+
+    print(images[1,:,:])
---
+++ ReadMNIST.py
@@ -0,0 +1,23 @@
+import struct
+import numpy as np
+
+def read_labels(path):
+    with open(path, 'rb') as f_labels:
+        magic, num = struct.unpack(">II", f_labels.read(8))
+        labels = np.fromfile(f_labels, dtype=np.int8)
+
+    return num, labels
+
+def read_images(path):
+    with open(path, 'rb') as f_images:
+        magic, num, rows, cols = struct.unpack(">IIII", f_images.read(16))
+        images = np.fromfile(f_images, dtype=np.uint8)
+        images.reshape(num, rows, cols)
+        
+    return num, images
+
+def label_to_binary_vector(lbl):
+    vec = np.zeros(10)
+    vec[lbl] = 1.0
+
+    return vec
